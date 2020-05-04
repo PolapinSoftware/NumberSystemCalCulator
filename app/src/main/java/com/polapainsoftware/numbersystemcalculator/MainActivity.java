17e653eba8c73f39e595b172740a17060ea17abd
anonymous <shakil.mezbah@gmail.com>
2020-05-04 23:27:46 +0600

    Initial Version

--- app/src/main/java/com/polapainsoftware/numbersystemcalculator/MainActivity.java
+++ app/src/main/java/com/polapainsoftware/numbersystemcalculator/MainActivity.java
@@ -3,12 +3,25 @@ package com.polapainsoftware.numbersystemcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText dec, bin, oct, hex;
+    int test = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dec = (EditText) findViewById(R.id.dec);
+        bin = (EditText) findViewById(R.id.bin);
+        oct = (EditText) findViewById(R.id.oct);
+        hex = (EditText) findViewById(R.id.hex);
+
+        dec.setText(test);
+
     }
 }
